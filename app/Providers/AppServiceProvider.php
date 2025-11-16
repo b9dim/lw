d3beff7559b35a82cf2f25b81f1a6cda9f8b4296
b9dim <b9dimm@gmail.com>
2025-11-16 08:01:43 +0300

    Simplify HTTPS URL handling in AppServiceProvider
    Removed conditional HTTPS URL handling and helper functions.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,8 @@
 
 namespace App\Providers;
 
-use App\Support\HttpsUrl;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,43 +14,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (file_exists(app_path('Helpers/ViteHelper.php'))) {
-            require_once app_path('Helpers/ViteHelper.php');
-        }
-
-        $forceHttps = HttpsUrl::shouldForce();
-
-        if ($forceHttps) {
-            URL::forceScheme('https');
-
-            if ($baseUrl = HttpsUrl::baseUrl()) {
-                URL::forceRootUrl($baseUrl);
-            }
-        }
-
-        if (!function_exists('force_https_asset')) {
-            function force_https_asset($path) {
-                $forced = \App\Support\HttpsUrl::asset($path);
-
-                if ($forced) {
-                    return $forced;
-                }
-
-                return secure_asset($path);
-            }
-        }
-
-        if (!function_exists('force_https_route')) {
-            function force_https_route($name, $parameters = [], $absolute = true) {
-                $forced = \App\Support\HttpsUrl::route($name, $parameters, $absolute);
-
-                if ($forced) {
-                    return $forced;
-                }
-
-                return route($name, $parameters, $absolute);
-            }
-        }
+        URL::forceScheme('https');
     }
 }
-
