9e30b6bd035537dc06dc72041aebf6dc5db4e8c1
b9dim <b9dimm@gmail.com>
2025-11-16 05:51:57 +0300

    99
    Added support for AWS ELB in the TrustProxies middleware.

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -7,17 +7,12 @@ use Illuminate\Http\Middleware\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
-    /**
-     * الثقة في جميع البروكسيات (Render يحتاج ذلك)
-     */
     protected $proxies = '*';
 
-    /**
-     * ضبط الهيدرات الخاصة بـ HTTPS
-     */
     protected $headers =
         Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
         Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO;
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
 }
